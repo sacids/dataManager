7a74e6e9981cc7e3a22c7f193aab8b26aa5590a6
Godluck Akyoo <g_akyoo@yahoo.com>
2016-02-23 15:01:56 +0300

    New configuration file

---
+++ application/config/sacids.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Godluck Akyoo
+ * Date: 2/22/2016
+ * Time: 1:02 PM
+ */
+
+
+$config['realm'] = "Authorized users of Sacids Openrosa";
+
+
+//Table names
+$config['table_xform']              = "xforms";
+$config['table_archive_xform']      = "archive_xforms";
+$config['table_form_submission']    = "submission_form";
+$config['table_feedback']           = "feedback";
+$config['table_users']              = "users";
+
+
+// Form uploads and Media files
+$config['form_definition_upload_dir']   = FCPATH. "assets/forms/definition/";
+$config['form_definition_archive_dir']  = FCPATH. "assets/forms/definition/archive/";
+$config['form_data_upload_dir']         = FCPATH. "assets/forms/data/xml/";
+$config['images_data_upload_dir']       = FCPATH. "assets/forms/data/images/";
+$config['audio_data_upload_dir']        = FCPATH. "assets/forms/data/audio/";
+$config['video_data_upload_dir']        = FCPATH. "assets/forms/data/video/";
+
+
+// Tables
