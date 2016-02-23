a145ac006610753a0d0b1750d72396ce52a7d508
Godluck Akyoo <g_akyoo@yahoo.com>
2016-02-23 12:14:35 +0300

    Implemented form edit and delete

--- application/models/Xform_model.php
+++ application/models/Xform_model.php
@@ -8,91 +8,144 @@
  */
 class Xform_model extends CI_Model
 {
-    /**
-     * Table name for xform definitions
-     * @var string
-     */
-    private static $xform_table_name = "xforms";
+	/**
+	 * Table name for xform definitions
+	 *
+	 * @var string
+	 */
+	private static $xform_table_name = "xforms";
 
-    /**
-     * @param $statement
-     * @return bool
-     */
-    public function create_table($statement)
-    {
+	/**
+	 * Table name for archived/deleted xforms
+	 *
+	 * @var string
+	 */
+	private static $archive_xform_table_name = "archive_xform";
 
-        if ($this->db->simple_query($statement)) {
-            log_message("debug", "Success!");
-            return TRUE;
-        } else {
-            $error = $this->db->error(); // Has keys 'code' and 'message'
-            log_message("debug", $statement . ", error " . json_encode($error));
-            return FALSE;
-        }
-    }
+	/**
+	 * @param $statement
+	 * @return bool
+	 */
+	public function create_table($statement)
+	{
 
-    /**
-     * @param $statement
-     * @return bool
-     */
-    public function insert_data($statement)
-    {
-        if ($this->db->simple_query($statement)) {
-            log_message("debug", "Success!");
-            return TRUE;
-        } else {
-            $error = $this->db->error(); // Has keys 'code' and 'message'
-            log_message("debug", $statement . ", error " . json_encode($error));
-        }
-    }
+		if ($this->db->simple_query($statement)) {
+			log_message("debug", "Success!");
+			return TRUE;
+		} else {
+			$error = $this->db->error(); // Has keys 'code' and 'message'
+			log_message("debug", $statement . ", error " . json_encode($error));
+			return FALSE;
+		}
+	}
 
-    /**
-     * @param $form_details an array of form details with keys match db field names
-     * @return mixed
-     */
-    public function create_xform($form_details)
-    {
-        return $this->db->insert(self::$xform_table_name, $form_details);
-    }
-    
-    /**
-     * @param int xform_id the row that needs to be updated
-     * @param string form_id 
-     * @return bool
-     */
-    public function update_form_id($xform_id, $form_id){
-    	
-    	$data	= array('form_id' => $form_id);
-    	$this->db->where('id',$xform_id);
-    	return $this->db->update('xforms',$data);
-    }
+	/**
+	 * @param $statement
+	 * @return bool
+	 */
+	public function insert_data($statement)
+	{
+		if ($this->db->simple_query($statement)) {
+			log_message("debug", "Success!");
+			return TRUE;
+		} else {
+			$error = $this->db->error(); // Has keys 'code' and 'message'
+			log_message("debug", $statement . ", error " . json_encode($error));
+		}
+	}
 
-    /**
-     * @param int $limit
-     * @param int $offset
-     * @return mixed returns list of forms available.
-     */
-    public function get_form_list($limit = 30, $offset = 0)
-    {
-        $this->db->limit($limit, $offset);
-        return $this->db->get(self::$xform_table_name)->result();
-    }
+	/**
+	 * @param $form_details an array of form details with keys match db field names
+	 * @return mixed
+	 */
+	public function create_xform($form_details)
+	{
+		return $this->db->insert(self::$xform_table_name, $form_details);
+	}
+
+	/**
+	 * @param int    xform_id the row that needs to be updated
+	 * @param string form_id
+	 * @return bool
+	 */
+	public function update_form_id($xform_id, $form_id)
+	{
+
+		$data = array('form_id' => $form_id);
+		$this->db->where('id', $xform_id);
+		return $this->db->update(self::$xform_table_name, $data);
+	}
+
+	/**
+	 * @param $form_id
+	 * @param $form_details
+	 * @return mixed
+	 */
+	public function update_form($form_id, $form_details)
+	{
+		$this->db->where('id', $form_id);
+		return $this->db->update(self::$xform_table_name, $form_details);
+	}
+
+	/**
+	 * @param int $limit
+	 * @param int $offset
+	 * @return mixed returns list of forms available.
+	 */
+	public function get_form_list($limit = 30, $offset = 0)
+	{
+		$this->db->limit($limit, $offset);
+		return $this->db->get(self::$xform_table_name)->result();
+	}
 
 
-    /**
-     * Finds a table field with point data type
-     * @param $table_name
-     * @return field name or FALSE
-     */
-    public function get_point_field($table_name)
-    {
+	/**
+	 * Finds a table field with point data type
+	 *
+	 * @param $table_name
+	 * @return field name or FALSE
+	 */
+	public function get_point_field($table_name)
+	{
 
-        $sql = " SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS ";
-        $sql .= " WHERE table_name = '{$table_name}' ";
-        $sql .= " AND DATA_TYPE = 'point'";
+		$sql = " SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS ";
+		$sql .= " WHERE table_name = '{$table_name}' ";
+		$sql .= " AND DATA_TYPE = 'point'";
 
-        $query = $this->db->query($sql);
+		$query = $this->db->query($sql);
 
-        return ($query->num_rows() == 1) ? $query->row(1)->COLUMN_NAME : FALSE;
-    }
+		return ($query->num_rows() == 1) ? $query->row(1)->COLUMN_NAME : FALSE;
+	}
+
+
+	/**
+	 * @param $form_id
+	 * @return mixed
+	 */
+	public function find_by_id($form_id)
+	{
+		$this->db->where("id", $form_id);
+		return $this->db->get(self::$xform_table_name)->row();
+	}
+
+	/**
+	 * @param $xform_id
+	 * @return mixed
+	 */
+	public function delete_form($xform_id)
+	{
+		$this->db->limit(1);
+		$this->db->where("id", $xform_id);
+		return $this->db->delete(self::$xform_table_name);
+	}
+
+
+	/**
+	 * @param $xform_archive_data
+	 * @return mixed
+	 */
+	public function create_archive($xform_archive_data)
+	{
+		return $this->db->insert(self::$archive_xform_table_name, $xform_archive_data);
+	}
 }
---
+++ application/views/form/edit_form.php
@@ -0,0 +1,38 @@
+<div class="grid_11" style="padding-top: 20px; text-align: left;">
+	<?php echo form_open_multipart('xform/edit_form/' . $form->id, 'class="pure-form pure-form-aligned"'); ?>
+	<div class="formCon">
+		<div class="formConInner">
+			<h3>Form Details</h3>
+			<?php
+			if ($this->session->flashdata('message') != '') {
+				echo '<div class="success_message">' . $this->session->flashdata('message') . '</div>';
+			} ?>
+
+			<fieldset>
+				<div class="pure-control-group">
+					<label>Form title <span>*</span></label>
+					<input type="text" name="title" placeholder="Enter Form Title" class="pure-input-1-2"
+					       value="<?php echo set_value('title', $form->title); ?>">
+				</div>
+				<div class="pure-form-message-inline"><?php echo form_error('title'); ?></div>
+
+				<div class="pure-control-group">
+					<label> <label for="campus">Description:</label> </label>
+                        <textarea class="pure-input-1-2" name="description"
+                                  id="description"><?php echo set_value('description', $form->description); ?></textarea>
+				</div>
+				<div class="pure-form-message-inline"><?php echo form_error('description'); ?></div>
+
+				<div class="pure-control-group">
+					<label>&nbsp; &nbsp; &nbsp;</label>
+					<button type="submit" class="pure-button pure-button-primary">Save</button>
+				</div>
+
+			</fieldset>
+		</div>
+	</div>
+	<?php echo form_close(); ?>
+</div>
+<div style="clear: both;"></div>
+</div>            </div>
+</div>
