2a5e9d533301fbbdb1a40f26b7e54750712dc41f
Godluck Akyoo <g_akyoo@yahoo.com>
2016-09-15 16:31:38 +0300

    Added option to Preview photo (enlarge)

--- application/views/form/form_data_details.php
+++ application/views/form/form_data_details.php
@@ -7,7 +7,7 @@
  */
 
 ?>
-<div class="container">
+<div class="container body-content">
     <div class="row">
         <div class="col-sm-12 col-md-12 col-lg-12 main">
             <div class="row" style="margin-bottom: 10px;">
@@ -83,4 +83,53 @@
     $("#selectAll").change(function () {
         $("input:checkbox").prop('checked', $(this).prop("checked"));
     });
+
+    $(function () {
+        var imageTitle;
+        var showModal = function () {
+
+            imageTitle = $(this).closest('tr').find('td:eq(5)').text();
+
+            var $input = $(this);
+            var imgAlt = $input.attr("alt");
+            $("#theModal h4.modal-title").html(imgAlt);
+            var img = this;
+            var imageHeight = $input.height();
+            var imagWidth = $input.width();
+            var NewimgWidth = imagWidth * 6;
+            var NewImgHeight = imageHeight * 6;
+            var picSrc = $input.attr("src");
+            $("#theModal img").attr('src', picSrc);
+            //set new image width
+            $("div.modal-dialog").css("width", NewimgWidth);
+            $("#theModal img").width(NewimgWidth);
+            //set new image height
+            $("#theModal img").height(NewImgHeight);
+            $("#theModal").modal("show");
+        };
+        var MyHtml = '<div id="theModal" class="modal fade">' +
+                ' <div class="modal-dialog ">' +
+                '<div class="modal-content">' +
+                ' <div class="modal-header">' +
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+                '<h4 class="modal-title">'+imageTitle+'</h4>' +
+                '</div>' +
+                '<div class="modal-body">' +
+                '  <img not-to-enlarge="true" class="img-responsive" + src=""alt="...">' +
+                '</div>' +
+                '<div class="modal-footer">' +
+                '<button type="button" class="btn btn-default" data-dismiss="modal">' +
+                'Close' +
+                '</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>';
+        $("div.body-content").append(MyHtml);
+        $("img[not-to-enlarge!=true]").click(showModal);
+        $("img[not-to-enlarge!=true]").css("cursor", "pointer");
+    });
+
+    $("img[not-to-enlarge!=true]").click(showModal);
+    $("img[not-to-enlarge!=true]").css("cursor", "pointer");
 </script>
