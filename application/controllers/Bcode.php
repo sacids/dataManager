6bba7c2ad7a00e4d8ba50768c556dc17cf521a69
Eric Beda <ericbeda@gmail.com>
2016-10-05 16:35:03 +0300

    Added barcode library and Lab request api

--- application/controllers/Bcode.php
+++ application/controllers/Bcode.php
@@ -9,20 +9,38 @@ class BCode extends CI_Controller{
 
         parent::__construct();
         $this->load->model(array('User_model', 'Submission_model', 'Campaign_model', 'Feedback_model'));
-        $this->load->library('Barcode');
+        $this->load->library('barcode');
         
         
     }
 
+    function create(){
+
+    }
+
     function display(){
 
-        print_r($this->Barcode);
-        echo $this->barcode->render("JAMBO");
-        return;
+        //print_r($this->barcode);
 
-        for($i = 100000; $i < 100050; $i++){
-            
-            echo $this->barcode->render("JAMBO");
+        $this->barcode->print = false;
+        //echo '<div></div><img src="'.base_url('assets/public').'/jj.png"></div>';
+        //return;
+
+        $bc = 'A001';
+        for($i = 0; $i < 50; $i++){
+
+            $this->barcode->filepath = FCPATH."assets/public/barcodes/".$bc.".png";
+            $this->barcode->render($bc);
+            echo    '<div style="width: 440px; margin: 10px; border: 2px solid #000; float: left; padding: 5px;">
+                        <div style="height: 65px; border-bottom: 2px solid #000;">&nbsp;</div>
+                        <div style="font-size:13px; text-align: center;">Jina Kamili</div>
+                        <div style="height: 50px; border-bottom: 2px solid #000;">&nbsp;</div>
+                        <div style="font-size:13px; text-align: center;">Namba ya usajili</div>
+                        <div style="padding: 5px; "><img style="margin-left: auto; margin-right: auto; display: block;" src="'.base_url('assets/public/barcodes/')."/".$bc.'.png"></div>
+                        <div style="font-size:17px; text-align: center;">'.$bc.'</div>
+                     </div>';
+
+            $bc++;
 
         }
 
--- application/controllers/api/v2/Lab.php
+++ application/controllers/api/v2/Lab.php
@@ -1,7 +1,49 @@
 <?php
+
 /**
  * Created by PhpStorm.
- * User: administrator
- * Date: 10/5/16
- * Time: 2:25 PM
+ * User: Renfrid-Sacids
+ * Date: 6/20/2016
+ * Time: 3:42 PM
  */
+class Lab extends CI_Controller
+{
+    function __construct(){
+        // load model
+        parent::__construct();
+        $this->load->model(array('User_model', 'Xform_model', 'Perm_model'));
+    }
+
+    public function requests(){
+
+        $key    = $this->input->get('k');
+        $val    = $this->input->get('v');
+        $tbl    = $this->input->get('t');
+
+        $res    = array();
+
+        if(empty($tbl)){
+            $tbl    = 'ad_build_Lab_Request_1475596584';
+        }
+        if(empty($key)){
+            $key    = '_xf_lrLt_2231';
+        }
+
+        if($data    = $this->Perm_model->get_table_data($tbl,$key,$val)){
+
+            $res['details'] = $data;
+            $res['status']  = 'success';
+            $res['form_id'] = 'build_Lab_Request_1475596584';
+
+        }else{
+
+            $res['status']  = 'failed';
+        }
+
+        echo json_encode($res);
+
+    }
+
+
+
+}
