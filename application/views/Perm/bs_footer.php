c07b316570fb4252d1895391e6896e8f1159dfbc
Eric Beda <ericbeda@gmail.com>
2016-10-31 11:04:08 +0300

    Changing PERM to bootstrap

---
+++ application/views/Perm/bs_footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: administrator
+ * Date: 10/12/16
+ * Time: 10:48 AM
+ */
---
+++ application/views/Perm/bs_head.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: administrator
+ * Date: 10/12/16
+ * Time: 11:02 AM
+ */
