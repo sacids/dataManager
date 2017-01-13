39f61053c45d2761f8049ce22e88c4ff0a86770a
Godluck Akyoo <g_akyoo@yahoo.com>
2017-01-13 15:58:56 +0300

    updated vendor path in shell script

--- bin/test.sh
+++ bin/test.sh
@@ -6,7 +6,7 @@ mysql -u $DB_USER -e "CREATE DATABASE afyadata_db_test;"
 
 CI_ENV=testing php index.php migration latest
 
-php application/vendor/kenjis/ci-phpunit-test/install.php
+php vendor/kenjis/ci-phpunit-test/install.php
 
 cd application/tests/
 phpunit --coverage-text
