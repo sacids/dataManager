4f2b72cb37f3e98cac88d4095353b3162a429c0c
Godluck Akyoo <g_akyoo@yahoo.com>
2017-01-14 00:25:06 +0300

    Changed db names to match defaults

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 DB_USER=${DB_USER:-root}
 
-mysql -u $DB_USER -e "DROP DATABASE IF EXISTS afyadata_db_test;"
-mysql -u $DB_USER -e "CREATE DATABASE afyadata_db_test;"
+mysql -u $DB_USER -e "DROP DATABASE IF EXISTS afyadata_test;"
+mysql -u $DB_USER -e "CREATE DATABASE afyadata_test;"
 
 CI_ENV=testing php index.php migration latest
 
