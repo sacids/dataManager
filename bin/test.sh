16c09e3966f936b904f356093857062b60042255
Godluck Akyoo <g_akyoo@yahoo.com>
2017-01-13 13:08:28 +0300

    Fixing test.sh script path issues

--- bin/test.sh
+++ bin/test.sh
@@ -6,11 +6,9 @@ mysql -u $DB_USER -e "CREATE DATABASE afyadata_db_test;"
 
 CI_ENV=testing php index.php migration latest
 
-pwd
+php application/vendor/kenjis/ci-phpunit-test/install.php
 
-php ./application/vendor/kenjis/ci-phpunit-test/install.php
-
-cd ./application/tests/
-../../bin/phpunit
+cd application/tests/
+phpunit
 
 eval "cd ../..; exit $?"
