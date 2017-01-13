3fc93386a45bb851362e66f8b62c537d76e6bc9e
Godluck Akyoo <g_akyoo@yahoo.com>
2017-01-13 12:40:33 +0300

    Added PHPUnit

--- bin/test.sh
+++ bin/test.sh
@@ -6,6 +6,9 @@ mysql -u $DB_USER -e "CREATE DATABASE afyadata_db_test;"
 
 CI_ENV=testing php index.php migration latest
 
+php application\vendor\kenjis\ci-phpunit-test\install.php
+
 cd application/tests/
-../../bin/phpunit
+#../../bin/phpunit
+phpunit
 eval "cd ../..; exit $?"
