6c65bc5bd5ec4d49d152acecdd22df3ebd14354e
Godluck Akyoo <g_akyoo@yahoo.com>
2017-01-13 11:04:26 +0300

    Added a test script

---
+++ bin/test.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+DB_USER=${DB_USER:-root}
+
+mysql -u $DB_USER -e "DROP DATABASE IF EXISTS afyadata_db_test;"
+mysql -u $DB_USER -e "CREATE DATABASE afyadata_db_test;"
+CI_ENV=testing php index.php migration latest
+cd application/tests/
+../../bin/phpunit
+eval "cd ../..; exit $?"
