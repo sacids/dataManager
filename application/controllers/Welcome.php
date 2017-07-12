b0ef551c60566b534bab4fd32631d4fdeb1660a8
Godluck Akyoo <g_akyoo@yahoo.com>
2017-07-12 13:57:01 +0300

    Fixed Welcome_test error

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,24 +1,26 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller
+class Welcome extends MX_Controller
 {
 
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     *        http://example.com/index.php/welcome
-     *    - or -
-     *        http://example.com/index.php/welcome/index
-     *    - or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see https://codeigniter.com/user_guide/general/urls.html
-     */
+	private $data;
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 *        http://example.com/index.php/welcome
+	 *    - or -
+	 *        http://example.com/index.php/welcome/index
+	 *    - or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 *
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
     public function index()
     {
         $this->data['title'] = 'Taarifa kwa wakati';
@@ -28,5 +30,4 @@ class Welcome extends CI_Controller
         $this->load->view('view');
         $this->load->view('layout/footer');
     }
-
 }
