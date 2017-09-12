bafba259d789084dfd944a3a8491dc6349433bbd
Godluck Akyoo <g_akyoo@yahoo.com>
2017-09-12 10:26:44 +0300

    Update migration file and session save path

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = (ENVIRONMENT == 'testing') ? 'dev_test' : 'default';
+$active_group = (ENVIRONMENT == 'testing' || ENVIRONMENT == 'development') ? 'dev_test' : 'default';
 //$active_group =  'dev_test';
 $query_builder = TRUE;
 
