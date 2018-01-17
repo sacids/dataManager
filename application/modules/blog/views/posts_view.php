60213462a9baa7785d95f571b27d3a4b4478e5b3
Renfrid Ngolongolo <renfridfrancis@gmail.com>
2018-01-17 10:42:36 +0300

    Customize blog view

--- application/modules/blog/views/posts_view.php
+++ application/modules/blog/views/posts_view.php
@@ -1,93 +1,96 @@
 <!-- Page Content -->
-<div class="container">
-    <div class="page-header">
-        <h5 class="section-title">
+<section class="page-wrapper">
+    <div class="container">
+        <div class="page-header">
+            <h5 class="section-title">
                 <span>
                     <a href="#">Newsletter Stories</a>
                 </span>
-        </h5>
-    </div>
+            </h5>
+        </div>
 
-    <div class="row">
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
-            <?php
-            if (isset($posts) && $posts) {
-                foreach ($posts as $post) { ?>
-                    <!-- Blog Post -->
-                    <div class="card mb-4">
-                        <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-                        <div class="card-body">
-                            <h4 class="card-title">
-                                <a href="<?= site_url('blog/post/post_details/' . $post->id) ?>"><?= $post->title ?></a>
-                            </h4>
-                            <p class="card-text"><?= strip_tags(substr($post->content, 0, 300)) ?></p>
-                            <a href="<?= site_url('blog/post/post_details/' . $post->id) ?>"
-                               class="btn btn-default btn-sm">Read More <i class="fa fa-angle-double-right"></i> </a>
+        <div class="row">
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+                <?php
+                if (isset($posts) && $posts) {
+                    foreach ($posts as $post) { ?>
+                        <!-- Blog Post -->
+                        <div class="card mb-4">
+                            <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="<?= site_url('blog/post/post_details/' . $post->id) ?>"><?= $post->title ?></a>
+                                </h4>
+                                <p class="card-text"><?= strip_tags(substr($post->content, 0, 300)) ?></p>
+                                <a href="<?= site_url('blog/post/post_details/' . $post->id) ?>"
+                                   class="btn btn-default btn-sm">Read More <i class="fa fa-angle-double-right"></i>
+                                </a>
+                            </div>
+                            <div class="card-footer text-muted">
+                                <?= 'Posted on ' . date("F d, Y g:i A", strtotime($post->date_created)) ?>
+                                by
+                                <a href="#"><?= $post->user->first_name . ' ' . $post->user->last_name ?></a>
+                            </div>
                         </div>
-                        <div class="card-footer text-muted">
-                            <?= 'Posted on ' . date("F d, Y g:i A", strtotime($post->date_created)) ?>
-                            by
-                            <a href="#"><?= $post->user->first_name . ' ' . $post->user->last_name ?></a>
-                        </div>
+                    <?php }
+                } ?>
+
+                <!-- Pagination -->
+                <?php if (!empty($links)): ?>
+                    <div class="widget-foot">
+                        <?= $links ?>
+                        <div class="clearfix"></div>
                     </div>
-                <?php }
-            } ?>
+                <?php endif; ?>
+            </div><!--./col-md-8 -->
 
-            <!-- Pagination -->
-            <?php if (!empty($links)): ?>
-                <div class="widget-foot">
-                    <?= $links ?>
-                    <div class="clearfix"></div>
-                </div>
-            <?php endif; ?>
-        </div><!--./col-md-8 -->
+            <!-- Sidebar Widgets Column -->
+            <div class="col-md-4">
+                <!-- Search Widget -->
+                <div class="panel panel-danger my-4">
+                    <div class="panel-heading">
+                        <i class="fa fa-search"></i>
+                        Search
+                    </div>
 
-        <!-- Sidebar Widgets Column -->
-        <div class="col-md-4">
-            <!-- Search Widget -->
-            <div class="panel panel-danger my-4">
-                <div class="panel-heading">
-                    <i class="fa fa-search"></i>
-                    Search
-                </div>
-
-                <div class="panel-body">
-                    <?= form_open('blog', 'role="form"') ?>
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search for...">
-                        <span class="input-group-btn">
+                    <div class="panel-body">
+                        <?= form_open('blog', 'role="form"') ?>
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for...">
+                            <span class="input-group-btn">
                             <button class="btn btn-primary" type="submit">Go!</button>
                         </span>
+                        </div>
+                        <?= form_close() ?>
                     </div>
-                    <?= form_close() ?>
-                </div>
-            </div><!--./panel -->
+                </div><!--./panel -->
 
-            <div class="panel panel-danger">
-                <div class="panel-heading">
-                    <i class="fa fa-info-circle"></i>
-                    Get in Touch
-                </div>
-                <div class="panel-body">
-                    <p>
-                        Are you interested in know more about AfyaData?
-                        Are you want to use AfyaData tool for data collection?
-                        Contact us at the number or email address below
-                    </p>
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <i class="fa fa-info-circle"></i>
+                        Get in Touch
+                    </div>
+                    <div class="panel-body">
+                        <p>
+                            Are you interested in know more about AfyaData?
+                            Are you want to use AfyaData tool for data collection?
+                            Contact us at the number or email address below
+                        </p>
 
-                    <p>
-                        <i class="fa fa-envelope hue"></i> <a
-                                href="mailto:afyadata@sacids.org">afyadata@sacids.org</a><br/>
-                        <i class="fa fa-phone hue"></i> +255 783 555 386
-                    </p>
-                </div>
-            </div><!--./panel -->
+                        <p>
+                            <i class="fa fa-envelope hue"></i> <a
+                                    href="mailto:afyadata@sacids.org">afyadata@sacids.org</a><br/>
+                            <i class="fa fa-phone hue"></i> +255 783 555 386
+                        </p>
+                    </div>
+                </div><!--./panel -->
 
-        </div><!--./col-md-4 -->
+            </div><!--./col-md-4 -->
+
+        </div>
+        <!-- /.row -->
 
     </div>
-    <!-- /.row -->
-
-</div>
-<!-- /.container -->
+    <!-- /.container -->
+</section>
--- application/modules/blog/views/single_post_view.php
+++ application/modules/blog/views/single_post_view.php
@@ -1,140 +1,143 @@
 <!-- Page Content -->
-<div class="container">
-    <div class="page-header">
-        <h5 class="section-title">
+<section class="page-wrapper">
+    <div class="container">
+        <div class="page-header">
+            <h5 class="section-title">
                 <span>
                     <a href="#">Newsletter Stories</a>
                 </span>
-        </h5>
-    </div>
-
-    <div class="row">
-        <!-- Post Content Column -->
-        <div class="col-lg-8">
-            <!-- Title -->
-            <h4 class="mt-4 text-primary"><?= (isset($post) ? $post->title : '') ?></h4>
-
-            <!-- Author -->
-            <p>Author :
-                <a href="#"><?= (isset($user) ? $user->first_name . ' ' . $user->last_name : 'Administrator') ?></a>
-            </p>
-            <hr>
-            <!-- Date/Time -->
-            <p><?= date("F d, Y g:i A", strtotime($post->date_created)) ?></p>
-            <hr>
-
-            <!-- Preview Image -->
-            <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
-            <hr>
-
-            <!-- Post Content -->
-            <?= $post->content ?>
-
-            <hr>
-            <!-- Comments Form -->
-            <div class="panel panel-light">
-                <div class="panel-heading">
-                    <i class="fa fa-comments-o"></i>
-                    Comments
-                </div>
-
-                <div class="panel-body">
-                    <div id="disqus_thread"></div>
-                    <script>
-
-                        /**
-                         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-                         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-                        /*
-                        var disqus_config = function () {
-                        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-                        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-                        };
-                        */
-                        (function () { // DON'T EDIT BELOW THIS LINE
-                            var d = document, s = d.createElement('script');
-                            s.src = 'https://afyadata.disqus.com/embed.js';
-                            s.setAttribute('data-timestamp', +new Date());
-                            (d.head || d.body).appendChild(s);
-                        })();
-                    </script>
-                    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments
-                            powered by Disqus.</a></noscript>
-                </div>
-            </div>
+            </h5>
         </div>
 
-        <!-- Sidebar Widgets Column -->
-        <div class="col-md-4">
-            <!-- Search Widget -->
-            <div class="panel panel-danger my-4">
-                <div class="panel-heading">
-                    <i class="fa fa-search"></i>
-                    Search
-                </div>
+        <div class="row">
+            <!-- Post Content Column -->
+            <div class="col-lg-8">
+                <!-- Title -->
+                <h4 class="mt-4 text-primary"><?= (isset($post) ? $post->title : '') ?></h4>
 
-                <div class="panel-body">
-                    <?= form_open('blog', 'role="form"') ?>
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search for...">
-                        <span class="input-group-btn">
-                            <button class="btn btn-primary" type="submit">Go!</button>
-                        </span>
+                <!-- Author -->
+                <p>Author :
+                    <a href="#"><?= (isset($user) ? $user->first_name . ' ' . $user->last_name : 'Administrator') ?></a>
+                </p>
+                <hr>
+                <!-- Date/Time -->
+                <p><?= date("F d, Y g:i A", strtotime($post->date_created)) ?></p>
+                <hr>
+
+                <!-- Preview Image -->
+                <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
+                <hr>
+
+                <!-- Post Content -->
+                <?= $post->content ?>
+
+                <hr>
+                <!-- Comments Form -->
+                <div class="panel panel-light">
+                    <div class="panel-heading">
+                        <i class="fa fa-comments-o"></i>
+                        Comments
                     </div>
-                    <?= form_close() ?>
-                </div>
-            </div><!--./panel -->
 
-            <div class="panel panel-danger">
-                <div class="panel-heading">
-                    <i class="fa fa-info-circle"></i>
-                    Get in Touch
-                </div>
-                <div class="panel-body">
-                    <p>
-                        Are you interested in know more about AfyaData?
-                        Are you want to use AfyaData tool for data collection?
-                        Contact us at the number or email address below
-                    </p>
+                    <div class="panel-body">
+                        <div id="disqus_thread"></div>
+                        <script>
 
-                    <p>
-                        <i class="fa fa-envelope hue"></i> <a
-                                href="mailto:afyadata@sacids.org">afyadata@sacids.org</a><br/>
-                        <i class="fa fa-phone hue"></i> +255 783 555 386
-                    </p>
-                </div>
-            </div><!--./panel -->
-
-            <!-- Begin fluid width widget -->
-            <div class="panel panel-danger">
-                <div class="panel-heading">
-                    <i class="fa fa-list"></i>
-                    Recent Posts
-                </div>
-                <div class="panel-body">
-                    <ul class="media-list">
-                        <?php if (isset($recent_posts) && $recent_posts) {
-                            foreach ($recent_posts as $p) { ?>
-                                <li class="media">
-                                    <div class="media-body">
-                                        <h6 class="media-heading">
-                                            <?= anchor("blog/post/post_details/" . $p->id, $p->title, 'class="text-primary"'); ?>
-                                        </h6>
-                                    </div>
-                                </li>
-                            <?php }
-                        } ?>
-
-                    </ul>
-                    <a href="<?= site_url('blog') ?>" class="btn btn-default btn-block">More Newsletter Stories »</a>
+                            /**
+                             *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                             *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                            /*
+                            var disqus_config = function () {
+                            this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                            this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                            };
+                            */
+                            (function () { // DON'T EDIT BELOW THIS LINE
+                                var d = document, s = d.createElement('script');
+                                s.src = 'https://afyadata.disqus.com/embed.js';
+                                s.setAttribute('data-timestamp', +new Date());
+                                (d.head || d.body).appendChild(s);
+                            })();
+                        </script>
+                        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments
+                                powered by Disqus.</a></noscript>
+                    </div>
                 </div>
             </div>
-            <!-- End fluid width widget -->
 
-        </div><!--./col-md-4 -->
+            <!-- Sidebar Widgets Column -->
+            <div class="col-md-4">
+                <!-- Search Widget -->
+                <div class="panel panel-danger my-4">
+                    <div class="panel-heading">
+                        <i class="fa fa-search"></i>
+                        Search
+                    </div>
+
+                    <div class="panel-body">
+                        <?= form_open('blog', 'role="form"') ?>
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search for...">
+                            <span class="input-group-btn">
+                            <button class="btn btn-primary" type="submit">Go!</button>
+                        </span>
+                        </div>
+                        <?= form_close() ?>
+                    </div>
+                </div><!--./panel -->
+
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <i class="fa fa-info-circle"></i>
+                        Get in Touch
+                    </div>
+                    <div class="panel-body">
+                        <p>
+                            Are you interested in know more about AfyaData?
+                            Are you want to use AfyaData tool for data collection?
+                            Contact us at the number or email address below
+                        </p>
+
+                        <p>
+                            <i class="fa fa-envelope hue"></i> <a
+                                    href="mailto:afyadata@sacids.org">afyadata@sacids.org</a><br/>
+                            <i class="fa fa-phone hue"></i> +255 783 555 386
+                        </p>
+                    </div>
+                </div><!--./panel -->
+
+                <!-- Begin fluid width widget -->
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <i class="fa fa-list"></i>
+                        Recent Posts
+                    </div>
+                    <div class="panel-body">
+                        <ul class="media-list">
+                            <?php if (isset($recent_posts) && $recent_posts) {
+                                foreach ($recent_posts as $p) { ?>
+                                    <li class="media">
+                                        <div class="media-body">
+                                            <h6 class="media-heading">
+                                                <?= anchor("blog/post/post_details/" . $p->id, $p->title, 'class="text-primary"'); ?>
+                                            </h6>
+                                        </div>
+                                    </li>
+                                <?php }
+                            } ?>
+
+                        </ul>
+                        <a href="<?= site_url('blog') ?>" class="btn btn-default btn-block">More Newsletter Stories
+                            »</a>
+                    </div>
+                </div>
+                <!-- End fluid width widget -->
+
+            </div><!--./col-md-4 -->
+
+        </div>
+        <!-- /.row -->
 
     </div>
-    <!-- /.row -->
-
-</div>
-<!-- /.container -->
+    <!-- /.container -->
+</section>
