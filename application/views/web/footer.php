43230da470fdee546809d9de2955e1f1fb8b0188
Júlio Rafael <33831855+jmrafael@users.noreply.github.com>
2021-05-06 09:11:21 +0200

    Revert "PT texts translations"
    This reverts commit 6b48e56a819c11c447909e60b7ad31f2b37a2e90.

--- application/views/web/footer.php
+++ application/views/web/footer.php
@@ -3,16 +3,16 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3 mb-5 mb-lg-0">
-                <h6 class="text-uppercase mb-4">Sobre Afyadata Moçambique</h6>
-                <p>É um conjunto de ferramentas de código aberto para colecta e submissão de dados sobre saúde baseados na comunidade e a respectiva retroalimentação da informação.</p>
+                <h6 class="text-uppercase mb-4">About Afyadata</h6>
+                <p>This is open source tools for collecting, submitting data from health facilities to the main server and receiving feedback from main server.</p>
                 <p>
                     <a href="<?= site_url('about') ?>"
-                       class="btn btn-primary btn-sm">Ler mais <i class="fa fa-angle-double-right"></i>
+                       class="btn btn-primary btn-sm">Read More <i class="fa fa-angle-double-right"></i>
                     </a></p>
             </div>
 
             <div class="col-md-3">
-                <h6 class="text-uppercase mb-4">Links Úteis</h6>
+                <h6 class="text-uppercase mb-4">Related Links</h6>
                 <p><i class="fa fa-angle-double-right"></i>&nbsp;&nbsp;
                     <a class="text-center" style="color: white !important;" href="http://sacids.org"
                        target="_blank">Sacids</a>
@@ -32,14 +32,10 @@
                     <a class="text-center" style="color: white !important;" href="http://www.skollglobalthreats.org"
                        target="_blank">Ending Pandemic</a>
                 </p>
-                <p><i class="fa fa-angle-double-right"></i>&nbsp;&nbsp;
-                    <a class="text-center" style="color: white !important;" href="https://www.ins.gov.mz"
-                       target="_blank">INS - Moçambique</a>
-                </p>
             </div>
 
             <div class="col-md-3 mb-5 mb-lg-0">
-                <h6 class="text-uppercase mb-4">Redes sociais</h6>
+                <h6 class="text-uppercase mb-4">Social Media Links</h6>
                 <ul class="list-inline mb-0">
                     <li class="list-inline-item">
                         <a class="btn btn-primary btn-social text-center rounded-circle"
@@ -60,12 +56,7 @@
             </div>
 
             <div class="col-md-3">
-                <h6 class="text-uppercase mb-4">Contactos</h6>
-                <strong>Moçambique</strong>
-                <p><i class="fa fa-envelope"></i>&nbsp;&nbsp;afyadata@ins.gov.mz </p>
-                <p><i class="fa fa-phone"></i>&nbsp;&nbsp; +258 </p>
-                 <hr />
-                <strong>Tanzania</strong>
+                <h6 class="text-uppercase mb-4">Contacts</h6>
                 <p><i class="fa fa-envelope"></i>&nbsp;&nbsp; afyadata@sacids.org </p>
                 <p><i class="fa fa-phone"></i>&nbsp;&nbsp; +255 783 555 386 </p>
 
@@ -80,12 +71,12 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="pull-left">
-                    <p>AfyaData Moçambique. &copy; <?= date('Y') ?> - Todos direitos reservados</p>
+                    <p>AfyaData. &copy; <?= date('Y') ?> - All Rights Reserved</p>
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="pull-right">
-                    <p>Desenvolvido por <a href="http://sacids.org" target="_blank">Southern African Centre for Infectious
+                    <p>Developed by <a href="http://sacids.org" target="_blank">Southern African Centre for Infectious
                             Disease Surveillance (SACIDS).</a></p>
                 </div>
             </div>
