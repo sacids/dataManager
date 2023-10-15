f0b0853ac8c235df5946e63f037e72bd776d93e6
renfrid <renfridfrancis@gmail.com>
2023-10-15 08:23:20 +0000

    log error

--- application/modules/xform/controllers/Xform.php
+++ application/modules/xform/controllers/Xform.php
@@ -1191,6 +1191,11 @@ class Xform extends MX_Controller
         $cases = $this->model->get_many_by(['form_id' => $form_id]);
         $this->data['cases'] = $cases;
 
+        echo "<pre/>";
+        print_r($cases);
+        exit();
+
+
         foreach($this->data['cases'] as $k => $v){
             $this->data['cases'][$k]->attended = $this->User_model->get_user_details($v->updated_by);
             $this->data['cases'][$k]->detected_disease = $this->Ohkr_model->get_disease_by_id($v->disease_id);
